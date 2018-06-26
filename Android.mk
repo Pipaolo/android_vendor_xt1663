8ff078061faf9ae6fdc0288f5266c85df31a18af
Pipaolo <mastastah8975@gmailcom>
2018-06-26 17:23:49 +0800

    Use built libaudio

--- Android.mk
+++ Android.mk
@@ -23,16 +23,6 @@ LOCAL_MODULE_SUFFIX := .so
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := libmtkcam_fwkutils
-LOCAL_SRC_FILES_64 := vendor/lib64/libmtkcam_fwkutils.so
-LOCAL_SRC_FILES_32 := vendor/lib/libmtkcam_fwkutils.so
-LOCAL_MULTILIB := both
-LOCAL_PROPRIETARY_MODULE := true
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := libdpframework
 LOCAL_SRC_FILES_64 := vendor/lib64/libdpframework.so
 LOCAL_SRC_FILES_32 := vendor/lib/libdpframework.so
